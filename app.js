4c8f2c6f649a74e8486d9f2a5b945afa7bc73c0a
gomathiCoder <91526592+gomathiCoder@users.noreply.github.com>
2022-04-23 17:16:29 +0530

    Update app.js
    Added date validation for February month.

--- app.js
+++ app.js
@@ -19,9 +19,15 @@ function emptyStringCheck() {
 }
 
 function dateValidation() {
-    if (!dob.value.match(/([0-2][0-9]|3[01])\/(0[0-9]|1[0-2])/)) {
-        warningDob.innerText = "Enter valid date in dd/mm format";
-    } else {
+    var date = dob.value.slice(0,2);
+    var month = dob.value.slice(-2);
+    if (!dob.value.match(/^([0-2][0-9]|3[01])\/(0[0-9]|1[0-2])$/)) {
+        dob.value = "";
+        warningDob.innerText = "Check date format*";
+    } else if((month == 2 && date > 29) || (date == 0) || (month == 0)){
+        warningDob.innerText = "Not valid date*";
+    }
+    else {
         warningDob.innerText = "";
         primeNumberCheck();
     }
@@ -38,9 +44,9 @@ function primeNumberCheck() {
         }
     }
     if (counter == 2) {
-        output.innerHTML = "It is a prime number"
+        output.innerHTML = "Your birthday and month is a prime number"
     } else {
-        output.innerHTML = "It is not a prime number";
+        output.innerHTML = "Your birthday and month is not a prime number";
     }
 }
 
